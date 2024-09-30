33ff6ea3819edc0bfb78265cefea4c4262466841
chikuma0 <chikuma@dera.ai>
2024-09-30 14:00:34 +0900

    Update build.js

--- build.js
+++ build.js
@@ -8,7 +8,7 @@ if (!fs.existsSync(distDir)){
 }
 
 let html = fs.readFileSync('index.html', 'utf8');
-html = html.replace('YOUR_API_KEY_PLACEHOLDER', process.env.GOOGLE_MAPS_API_KEY);
+html = html.replace('YOUR_API_KEY_PLACEHOLDER', process.env.AIzaSyCCQsM6IW_drrcpvIUBB545G9j4rUGVvIE);
 fs.writeFileSync(path.join(distDir, 'index.html'), html);
 
 console.log('API Key replaced successfully'); // Add this for debugging
