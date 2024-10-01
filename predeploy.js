ed95c48184956efec94381cc22db44f8bfd1d11b
chikuma0 <chikuma0@users.noreply.github.com>
2024-10-01 00:33:30 +0000

    deploy: 08d344d26251a198b3913d40fc0fe81c7563632a

--- predeploy.js
+++ predeploy.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 try {
   let html = fs.readFileSync('index.html', 'utf8');
-  html = html.replace('YOUR_API_KEY_PLACEHOLDER', process.env.AIzaSyCCQsM6IW_drrcpvIUBB545G9j4rUGVvIE);
+  html = html.replace('YOUR_API_KEY_PLACEHOLDER', process.env.GOOGLE_MAPS_API_KEY);
   fs.writeFileSync('dist/index.html', html);
   console.log('Pre-deployment script completed successfully.');
 } catch (error) {
